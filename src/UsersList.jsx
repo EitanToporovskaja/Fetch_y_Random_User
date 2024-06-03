d33dcdc16ecc59155a32b50fb318a033c8c8ec7d
eitan <mail@mail.com>
2024-06-03 15:37:22 -0300

    hola

--- src/UsersList.jsx
+++ src/UsersList.jsx
@@ -2,21 +2,31 @@ import { useState,useEffect }from "react"
 
 const UsersList = () =>{
 
-    const [users, setUsers]= useState ([])
+    const [users, setUsers] = useState ([])
+    const [ubicacion, setUbicacion] = useState([])
     const paginaApi="https://randomuser.me/api/?results=5"
 
     useEffect(() => {
-        fetch (paginaApi)
+        fetch (paginaApi)//busca la pagina
             .then(response => response.json())//hace esto y despues lo siguiente. si falla da catch error
-            .then(data => setUsers(data.results))
+            .then(data => setUsers(data.results))//define el estado de los usuarios
+            .then(data => setUbicacion(data.results))
             .catch(error => console.log(error))
-    }, [users])
+    }, [])
 
     return (
         <div>
             <h1>Listado de usuarios: </h1>
-            <ul>
+            <ul> 
                 {users.map(user => <li>{user.name.first}</li>)}
+            </ul>
+            <ul> 
+                {ubicacion.map(ubicacion => <li>{ubicacion.picture.first}</li>)}
+            </ul>
+            <ul> 
+                {users.map(user => <li>{user.picture.first}</li>)}
+            </ul>  <ul> 
+                {users.map(user => <li>{user.picture.first}</li>)}
             </ul>
         </div>
     )
